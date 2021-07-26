79048ca2b2d4d2e3a190c9b200efbd47dbd8aee3
Vladyslav Mortikov <mortikov60@gmail.com>
2021-07-26 10:18:51 +0300

    Delete config.js

--- sch2.hp app/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-        ServerPort: process.env["PORT"] || 2345,
-        DatabaseUrl: process.env["DATABASE_URL"] || 'mongodb://localhost:27017/lab5db',
-        cloud_name: process.env["CLOUD_NAME"] || 'sch2hpcloud',
-        api_secret: process.env["API_SECRET"] || 'TF7fAN1kXTxJd9qHNDRrhIkbUt8',
-        api_key: process.env["API_KEY"] || '374631992754542',
-};
